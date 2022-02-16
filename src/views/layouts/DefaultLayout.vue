53bb7c01bfcfe2b0f8a38fc95295b3f143f089f5
sjtuli <lyha23@sjtu.edu.cn>
2022-02-16 12:19:18 +0800

    建立远程仓库

---
+++ src/views/layouts/DefaultLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+    <!-- <img alt="Vue logo" src="@/assets/logo.png">   -->
+    <NavBar msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import AppMain from '@/views/layouts/components/AppMain.vue'
+import NavBar from '@/views/layouts/components/NavBar.vue'
+
+import '@/assets/css/font-icon.min.css'
+import '@/assets/css/bootstrap.min.css'
+import '@/assets/css/reset.css'
+import '@/assets/css/defined.css'
+import '@/assets/css/base.css'
+import '@/assets/css/style.css'
+import '@/assets/css/home-decor.css'
+
+export default {
+  name: 'Home',
+  components: {
+    NavBar,
+    // AppMain
+  }
+}
+</script>
