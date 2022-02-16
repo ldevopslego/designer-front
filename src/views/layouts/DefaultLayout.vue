2177748c4bf0422864ae584e031407ee8402c86a
sjtuli <lyha23@sjtu.edu.cn>
2022-02-16 13:24:50 +0800

    body 样式

--- src/views/layouts/DefaultLayout.vue
+++ src/views/layouts/DefaultLayout.vue
@@ -1,9 +1,13 @@
 <template>
   <div >
     <!-- <img alt="Vue logo" src="@/assets/logo.png">   -->
+    <body class="header_full_true kalles-template css_scrollbar lazy_icons btnt4_style_2 zoom_tp_2 css_scrollbar template-index kalles_toolbar_true hover_img2 swatch_style_rounded swatch_list_size_small label_style_rounded wrapper_full_width header_full_true header_sticky_true hide_scrolld_true des_header_3 h_banner_true top_bar_true prs_bordered_grid_1 search_pos_canvas lazyload">
+    <div id="nt_wrapper">
     <NavBar msg="Welcome to Your Vue.js App"/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
     <Footer msg="Welcome to Your Vue.js App"/>
+    </div>
+    </body>
   </div>
 </template>
 
